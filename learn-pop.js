0b956f11b99bc0b6538fadb063481d91362c041f
Matt Wyndham <matthewwyndham@gmail.com>
2019-04-19 09:23:22 -0600

    finished pop, started shift

--- learn-pop.js
+++ learn-pop.js
@@ -1,3 +1,17 @@
 // learn pop from mdn 
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/pop
 
+// 1. removes the last element of the array
+// 2. destructive
+// 3. returns the last element of the array (undefined if empty)
+// 4. if you get user input from the command line and then split on spaces you'll still have a newline character at the end. You can remove it with pop
+// 5.
+var userInput = "I love to have lemons \n"
+console.log(userInput)
+
+var splitOnSpace = userInput.split(" ")
+console.log(splitOnSpace)
+
+var splitInput = splitOnSpace.pop()
+console.log(splitOnSpace)
+console.log(splitInput)
--- learn-shift.js
+++ learn-shift.js
@@ -1,7 +1,7 @@
-Questions to understand:
+// learn shift from mdn 
 /* 1. What does it do? make sure to explain all the parameters. If it has a function as a parameter, make sure to explain all of the parameters for that function.
 *
-*
+* Given an array, shift will remove the first element of the array, return it, and reduce the length of the array.
 */
 
 /* 2. Does it edit the current array?
