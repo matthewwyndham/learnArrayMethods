b4006c3832dc5aed080ca73a53dea80bbb168a89
Matt Wyndham <matthewwyndham@gmail.com>
2019-04-22 13:54:15 -0600

    fill optimization

--- learn-fill.js
+++ learn-fill.js
@@ -22,48 +22,83 @@ const { PerformanceObserver, performance } = require('perf_hooks');
 "after testing though, it appears the fill function actually takes much longer."
 
 /* 5. Build your real world example. */
-var fillarray = [1, 2, 3, 4, 5];
-var looparray = [1, 2, 3, 4, 5];
-var anewarray = [1, 2, 3, 4, 5];
-var newfarray = [1, 2, 3, 4, 5];
-var t0, t1, t2, t3, t4, t5, t6;
+testMethods(10);
+testMethods(100);
+testMethods(1000);
+testMethods(10000);
+testMethods(100000);
 
-console.log(fillarray);
+function testMethods(numberOfItems) {
+    var t0, t1, t2, t3, t4, t5, t6, t7; // timers
 
-t0 = performance.now(); // timer
+    var fillarray = Array(numberOfItems).fill(1);
+    var looparray = Array(numberOfItems).fill(1);
+    var anewarray = Array(numberOfItems).fill(1);
+    var newfarray = Array(numberOfItems).fill(1);
+    var mapparray = Array(numberOfItems).fill(1);
+    var shpuarray = Array(numberOfItems).fill(1);
+    var pousarray = Array(numberOfItems).fill(1);
 
-// fill
-// clear the array (supposedly fast)
-fillarray.fill(0); 
+    console.log(`array of length ${numberOfItems} (in milliseconds)`);
 
-t1 = performance.now();
+    t0 = performance.now();
 
-// for-loop
-// a regular for loop to clear the array
-for (let i = 0; i < looparray.length; ++i) { 
-    looparray[i] = 0;
+    // fill
+    fillarray.fill(0); 
+
+    t1 = performance.now();
+
+    // for-loop
+    for (let i = 0; i < looparray.length; ++i) { 
+        looparray[i] = 0;
+    }
+
+    t2 = performance.now();
+
+    // constructor & push loop
+    let anewarrayLength = anewarray.length;
+    anewarray = Array();
+    for(let i = 0; i < anewarrayLength; ++i) {
+        anewarray.push(0);
+    }
+
+    t3 = performance.now();
+
+    // constructor & fill
+    newfarray = Array(newfarray.length).fill(0);
+
+    t4 = performance.now();
+
+    // map
+    mapparray = mapparray.map(x => 0)
+
+    t5 = performance.now();
+
+    // shift and push
+    let shpuarrayLength = shpuarray.length;
+    for(let i = 0; i < shpuarrayLength; ++i) {
+        shpuarray.shift();
+        shpuarray.push(0);
+    }
+
+    t6 = performance.now();
+
+    // pop and unshift
+    let pousarrayLength = pousarray.length;
+    for(let i = 0; i < pousarrayLength; ++i) {
+        pousarray.pop();
+        pousarray.unshift(0);
+    }
+
+    t7 = performance.now();
+
+    console.log(`>>>
+    ${(t1 - t0).toFixed(5)} : fill
+    ${(t2 - t1).toFixed(5)} : for-loop
+    ${(t3 - t2).toFixed(5)} : constructor & push loop
+    ${(t4 - t3).toFixed(5)} : constructor & fill
+    ${(t5 - t4).toFixed(5)} : map
+    ${(t6 - t5).toFixed(5)} : shift and push
+    ${(t7 - t6).toFixed(5)} : pop and unshift
+    `);
 }
-
-t2 = performance.now();
-
-// constructor & push loop
-// create a new array and then push a certain number of zeros on there
-let anewarrayLength = anewarray.length;
-anewarray = Array();
-for(let i = 0; i < anewarrayLength; ++i) {
-    anewarray.push(0)
-}
-
-t3 = performance.now();
-
-// constructor & fill
-// constructor with length of original array, then fill with 0
-newfarray = Array(newfarray.length).fill(0);
-
-t4 = performance.now();
-
-console.log(`(in milliseconds)
-${fillarray} : ${(t1 - t0).toFixed(5)} : fill
-${looparray} : ${(t2 - t1).toFixed(5)} : for-loop
-${anewarray} : ${(t3 - t2).toFixed(5)} : constructor & push loop
-${newfarray} : ${(t4 - t3).toFixed(5)} : constructor & fill`);
