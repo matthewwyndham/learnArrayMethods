202bddfa47e1375977c910632aabb4f81f87f4e4
Matt Wyndham <matthewwyndham@gmail.com>
2019-04-22 09:37:31 -0600

    learned reduce

--- learn-reduce.js
+++ learn-reduce.js
@@ -13,6 +13,36 @@
 "the accumulated value from each iteration"
 
 /* 4. How can I use this? Come up (not one off the internet) with a small real world example and explain it. */
-
+"If I have an array of words and I want to know the total length of each word in the array, added together, then I could use a reduce"
 
 /* 5. Build your real world example. */
+
+// with a starting accumulator value
+var myArray = ["apple", "banana", "cucumber", "lemon", "sock", "cow"];
+
+function countLetters(word) {
+    return word.length;
+}
+
+var reducer = (accu, element) => accu + countLetters(element);
+
+console.log(myArray.reduce(reducer, 0));
+
+// without a starting accumulator value
+var myArray2 = ["apple", "banana", "cucumber", "lemon", "sock", "cow"];
+
+// using countLetters from before
+
+// var reducer2 = (accu2, element2) => accu2 + countLetters2(element2);
+function reducer2(a, e) {
+    var summ = 0;
+    if (typeof a == "string") { // this handles the initial case where the first element is used
+        summ = a.length;        // as the starting value.
+    }
+    else {
+        summ = a;
+    }
+    return summ + countLetters(e);
+}
+
+console.log(myArray2.reduce(reducer2));
